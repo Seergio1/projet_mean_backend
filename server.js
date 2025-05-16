f59e7093058ac7d7044a0fc19af5606dc96272ce
Sergio <sergiorajaohariniaina@gmail.com>
2025-05-16 13:45:41 +0300

    version complet 1.2.1

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   'http://192.168.0.36:5000',
-  'https://projet-mean-frontend.vercel.app'
+  'https://m1p12mean-sergio-dimby.vercel.app'
 ];
 
 // Middleware
