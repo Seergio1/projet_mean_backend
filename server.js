06a638d1134df117e32dc972acb0232c62d5d4f8
Sergio <sergiorajaohariniaina@gmail.com>
2025-05-19 14:53:05 +0300

    modification puppeter 5

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const Tache = require('./models/Tache');
 const stockService = require('./services/StockService');
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+process.env.PUPPETEER_CACHE_DIR = '/tmp/puppeteer-cache';
 
 
 const allowedOrigins = [
